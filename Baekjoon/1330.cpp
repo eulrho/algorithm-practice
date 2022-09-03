bbeee60c785ce1cad1df97138fa3843993762207
Eul Rho <sunriselss19@gmail.com>
2022-09-03 13:27:52 +0900

    1330. 두 수 비교하기

---
+++ Baekjoon/1330.cpp
@@ -0,0 +1,16 @@
+// 두 수 비교하기
+// 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a, b;
+	cin >> a >> b;
+
+	if (a > b) cout << '>';
+	else if (a < b) cout << '<';
+	else cout << "==";
+
+	return 0;
+}
