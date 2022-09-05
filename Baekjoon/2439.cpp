9965b623ff89835f86c5d56eb4a1a2f171a4354e
Eul Rho <sunriselss19@gmail.com>
2022-09-05 16:24:07 +0900

    2439. 별 찍기-2

---
+++ Baekjoon/2439.cpp
@@ -0,0 +1,17 @@
+// 별 찍기-2
+// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cout.width(n-i+1);
+		for (int j = 1; j <= i; j++) {
+			cout << '*';
+		}
+		cout << endl;
+	}
+}
