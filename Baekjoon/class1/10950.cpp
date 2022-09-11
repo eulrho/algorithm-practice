e2ff3e7b509e30b83a331b9e309a39484e70d09a
Eul Rho <sunriselss19@gmail.com>
2022-09-11 11:56:07 +0900

    10950. A + B -3

---
+++ Baekjoon/class1/10950.cpp
@@ -0,0 +1,16 @@
+// A + B -3
+// 두 정수 A와 B를 입력받은 다음, A + B를 출력하는 프로그램을 작성하시오.
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t, a, b;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b;
+		cout << a + b << endl;;
+	}
+	
+	return 0;
+}
