21e4ea0ab0dce6421cbd92f679fc425e05df3aa9
eulrho <sunriselss19@gmail.com>
2022-10-01 23:36:07 +0900

    2743. 단어 길이 재기

---
+++ Baekjoon/bronze5/2743.cpp
@@ -0,0 +1,15 @@
+// 단어 길이 재기
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string str;
+	cin >> str;
+	cout << str.length();
+	return 0;
+}
