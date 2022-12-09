76bd59cab91836a8bc167829e4d2e8e8bc93c3c0
eulrho <sunriselss19@gmail.com>
2022-12-09 23:35:22 +0900

    15964. 이상한 기호

---
+++ Baekjoon/bronze5/15964.cpp
@@ -0,0 +1,12 @@
+// 이상한 기호
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long a, b;
+    cin >> a >> b;
+    cout << (a+b)*(a-b);
+    return 0;
+}
