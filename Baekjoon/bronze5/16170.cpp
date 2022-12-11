8f2410174c78eb96ba5a5d2a52286f8d5f614062
eulrho <sunriselss19@gmail.com>
2022-12-11 22:34:52 +0900

    16170. 오늘의 날짜는?

---
+++ Baekjoon/bronze5/16170.cpp
@@ -0,0 +1,11 @@
+// 오늘의 날짜는?
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "2022\n" << "12\n" << "11";
+    return 0;
+}
