6092687ce29502d078162a16a8d79f77da9d511d
eulrho <sunriselss19@gmail.com>
2024-03-02 16:58:56 +0900

    14681. 사분면 고르기

---
+++ Baekjoon/bronze/14681.cpp
@@ -0,0 +1,17 @@
+// 사분면 고르기
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int x, y;
+
+	cin >> x >> y;
+	if (x > 0 && y > 0) cout << 1;
+	else if (x < 0 && y > 0) cout << 2;
+	else if (x > 0 && y < 0) cout << 4;
+	else cout << 3;
+	return (0);
+}
