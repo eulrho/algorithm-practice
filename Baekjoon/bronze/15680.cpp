061f9712a3f66076432a90ec3f12f1dcf5e560d3
eulrho <sunriselss19@gmail.com>
2024-03-05 12:25:21 +0900

    15680. 연세대학교

---
+++ Baekjoon/bronze/15680.cpp
@@ -0,0 +1,17 @@
+// 연세대학교
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n;
+
+	cin >> n;
+	if (n == 1)
+		cout << "Leading the Way to the Future";
+	else
+		cout << "YONSEI";
+	return (0);
+}
