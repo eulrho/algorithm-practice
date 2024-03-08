3eeb64c4bcb78d1df82320cb280c00e15cce91d3
eulrho <sunriselss19@gmail.com>
2024-03-08 14:57:43 +0900

    15963. CASIO

---
+++ Baekjoon/bronze/15963.cpp
@@ -0,0 +1,15 @@
+// CASIO
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	long long n, m;
+
+	cin >> n >> m;
+	if (n == m) cout << 1;
+	else cout << 0;
+	return (0);
+}
