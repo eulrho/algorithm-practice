f395b0c5dceb8f5ea0ef4d6b7f59ff12556f5e3e
eulrho <sunriselss19@gmail.com>
2024-03-11 14:21:49 +0900

    16394. 홍익대학교

---
+++ Baekjoon/bronze/16394.cpp
@@ -0,0 +1,14 @@
+// 홍익대학교
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int year;
+
+	cin >> year;
+	cout << (year - 1946);
+	return (0);
+}
