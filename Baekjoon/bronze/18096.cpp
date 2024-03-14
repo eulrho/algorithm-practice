bf1abd59d0192dead2dc6243944b32182bc74d14
eulrho <sunriselss19@gmail.com>
2024-03-14 16:02:09 +0900

    18096. Арифметическая магия(숫자 마술)

---
+++ Baekjoon/bronze/18096.cpp
@@ -0,0 +1,14 @@
+// Арифметическая магия(숫자 마술)
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n;
+
+	cin >> n;
+	cout << 1;
+	return (0);
+}
