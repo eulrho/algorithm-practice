92a60a9b66116bd23905b87656569f7e02a98bf3
eulrho <sunriselss19@gmail.com>
2024-03-16 08:31:34 +0900

    18108. 1998년생인 내가 태국에서는 2541년생?!

---
+++ Baekjoon/bronze/18108.cpp
@@ -0,0 +1,14 @@
+// 1998년생인 내가 태국에서는 2541년생?!
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int y;
+
+	cin >> y;
+	cout << y - 543;
+	return (0);
+}
