7e22f8ba7a5d27dec19cf4d540e76969fa8dd482
eulrho <sunriselss19@gmail.com>
2024-03-20 22:00:34 +0900

    20254. Site Score

---
+++ Baekjoon/bronze/20254.cpp
@@ -0,0 +1,14 @@
+// Site Score
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int Ur, Tr, Uo, To;
+
+	cin >> Ur >> Tr >> Uo >> To;
+	cout << 56 * Ur + 24 * Tr + 14 * Uo + 6 * To;
+	return (0);
+}
