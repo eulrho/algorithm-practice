19855fd0cbc989e3d4c66def95ac2921aeb12c0f
eulrho <sunriselss19@gmail.com>
2024-03-23 16:53:31 +0900

    21598. SciComLove

---
+++ Baekjoon/bronze/21598.cpp
@@ -0,0 +1,14 @@
+// SciComLove
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n;
+
+	cin >> n;
+	for (int i=0; i<n; i++) cout << "SciComLove\n";
+	return (0);
+}
