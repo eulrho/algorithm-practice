a43cdf143105c04aa30210c9d7370b035902c565
eulrho <sunriselss19@gmail.com>
2024-03-25 15:47:47 +0900

    23234. The World Responds

---
+++ Baekjoon/bronze/23234.cpp
@@ -0,0 +1,11 @@
+// The World Responds
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	cout << "The world says hello!";
+	return (0);
+}
