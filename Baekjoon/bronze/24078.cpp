02a11818ae696d171ec2898de5905e5262a90ee5
eulrho <sunriselss19@gmail.com>
2024-03-27 16:48:35 +0900

    24078. 余り (Remainder)

---
+++ Baekjoon/bronze/24078.cpp
@@ -0,0 +1,14 @@
+// 余り (Remainder)
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int x;
+
+	cin >> x;
+	cout << x % 21;
+	return (0);
+}
