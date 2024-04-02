b5b096c849ab9ad349721042824c9c7493196822
eulrho <sunriselss19@gmail.com>
2024-04-02 12:38:32 +0900

    24568. Cupcake Party

---
+++ Baekjoon/bronze/24568.cpp
@@ -0,0 +1,14 @@
+// Cupcake Party
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int r, s;
+
+	cin >> r >> s;
+	cout << r * 8 + s * 3 - 28;
+	return (0);
+}
