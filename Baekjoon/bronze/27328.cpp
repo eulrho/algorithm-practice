6f8078206c3e651573e2dc6b586ae24e5b6225e0
eulrho <sunriselss19@gmail.com>
2024-04-29 18:28:02 +0900

    27328. 三方比較 (Three-Way Comparison)

---
+++ Baekjoon/bronze/27328.cpp
@@ -0,0 +1,16 @@
+// 三方比較 (Three-Way Comparison)
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int a, b;
+
+	cin >> a >> b;
+	if (a < b) cout << -1;
+	else if (a == b) cout << 0;
+	else cout << 1;
+	return (0);
+}
