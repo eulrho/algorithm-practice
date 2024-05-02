f726a57f14f8faa05d5711f133f5677c9c03781b
eulrho <sunriselss19@gmail.com>
2024-05-02 23:51:23 +0900

    27389. Metronome

---
+++ Baekjoon/bronze/27389.cpp
@@ -0,0 +1,16 @@
+// Metronome
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	double n;
+
+	cin >> n;
+	cout << fixed;
+	cout.precision(2);
+	cout << n / 4;
+	return (0);
+}
