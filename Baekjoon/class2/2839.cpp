c5962f32217d8024469691bbc6c2888082c4093e
eulrho <sunriselss19@gmail.com>
2024-07-03 16:45:01 +0900

    2839. 설탕 배달(ver.dp)

--- Baekjoon/class2/2839.cpp
+++ Baekjoon/class2/2839.cpp
@@ -1,25 +1,27 @@
 // 설탕 배달
 #include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 using namespace std;
 
-int main() {
-	int n, count=2000, i=0, res; cin >> n;
-	bool success = false;
-	while(i<=1666){
-		for (int j = 0; j <= 1000; j++) {
-			res = i * 3 + j * 5;
-			if (res == n && count > i + j) {
-				count = i + j;
-				success = true;
-			}
-			if (res >= n) break;
+int dp[5001];
+
+int main()
+{
+	fast;
+	int n;
+
+	cin >> n;
+	for (int i=1; i<=n; i++) {
+		if (i >= 3 && i % 3 == 0) dp[i] = i / 3;
+		if (i >= 5) {
+			if (i % 5 == 0) dp[i] = i / 5;
+			else if (dp[i] != 0 && dp[i - 5] != 0)
+				dp[i] = min(dp[i], dp[i - 5] + 1);
+			else if (dp[i] == 0 && dp[i - 5] != 0)
+				dp[i] = dp[i - 5] + 1;
 		}
-		if (i * 3 >= n) break;
-		else i++;
 	}
-	
-	if (success == false) cout << -1;
-	else cout << count;
-		
-	return 0;
+	if (dp[n] == 0) cout << -1;
+	else cout << dp[n];
+	return (0);
 }
