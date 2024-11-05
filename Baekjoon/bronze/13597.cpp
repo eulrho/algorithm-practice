f99bfde0e030c3f224d8102aedca7e4b8881a1d0
eulrho <sunriselss19@gmail.com>
2024-11-05 09:49:15 +0900

    13597. Tri-du

---
+++ Baekjoon/bronze/13597.cpp
@@ -0,0 +1,14 @@
+// Tri-du
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int a, b;
+
+	cin >> a >> b;
+	cout << max(a, b);
+	return (0);
+}
