090fca068dcefe7b7f59c29794d2fc07b7d290b6
eulrho <sunriselss19@gmail.com>
2025-01-14 00:56:28 +0900

    15439. 베라의 패션

---
+++ Baekjoon/bronze/15439.cpp
@@ -0,0 +1,12 @@
+// 베라의 패션
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n; cin >> n;
+	cout << n * n - n;
+	return (0);
+}
