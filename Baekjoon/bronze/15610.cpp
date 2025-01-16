7cabfd814727168a02b5ba6e32db5605ca5c9f0c
eulrho <sunriselss19@gmail.com>
2025-01-17 00:46:31 +0900

    15610. Abbey Courtyard

---
+++ Baekjoon/bronze/15610.cpp
@@ -0,0 +1,15 @@
+// Abbey Courtyard
+#include <iostream>
+#include <cmath>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	double a; cin >> a;
+	cout.precision(6);
+	cout << fixed;
+	cout << sqrt(a) * 4;
+	return (0);
+}
