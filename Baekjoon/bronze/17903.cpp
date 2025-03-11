8e94acadcd7c7a91720ccac21217b431fb2cc1a2
eulrho <sunriselss19@gmail.com>
2025-03-12 00:45:03 +0900

    17903. Counting Clauses

---
+++ Baekjoon/bronze/17903.cpp
@@ -0,0 +1,13 @@
+// Counting Clauses
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int m; cin >> m;
+	if (m >= 8) cout << "satisfactory";
+	else cout << "unsatisfactory";
+	return (0);
+}
