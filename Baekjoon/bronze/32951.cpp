2bbdbf904675cdd26b157f7c2b3c6592e4660b59
eulrho <sunriselss19@gmail.com>
2025-04-02 11:05:22 +0900

    AI 선도대학

---
+++ Baekjoon/bronze/32951.cpp
@@ -0,0 +1,12 @@
+// AI 선도대학
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n; cin >> n;
+	cout << n - 2024;
+	return (0);
+}
