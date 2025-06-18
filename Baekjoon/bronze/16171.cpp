11d0504c29a0ab0232036ede8ec99e917327955f
eulrho <sunriselss19@gmail.com>
2025-06-18 16:20:43 +0900

    16171. 나는 친구가 적다 (Small)

---
+++ Baekjoon/bronze/16171.cpp
@@ -0,0 +1,18 @@
+// 나는 친구가 적다 (Small)
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	string s, k; cin >> s >> k;
+	string tmp;
+	for (int i=0; i<(int)s.size(); i++) {
+		if (s[i] >= '0' && s[i] <= '9') continue ;
+		tmp += s[i];
+	}
+	cout << (tmp.find(k) != string::npos);
+	return (0);
+}
