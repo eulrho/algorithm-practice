b772e3670a235c3cdece1393192df59be4c2bc14
eulrho <sunriselss19@gmail.com>
2025-07-20 17:58:48 +0900

    15858. Simple Arithmetic

---
+++ Baekjoon/bronze/15858.cpp
@@ -0,0 +1,18 @@
+// Simple Arithmetic
+#include <iostream>
+#include <string>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	long long a, b, c;
+	cin >> a >> b >> c;
+	long long tmp = a * b / c;
+
+	string result = to_string(tmp);
+	string decimal = to_string((double)(a * b - tmp * c) / c);
+	cout << result + decimal.substr(1);
+	return (0);
+}
