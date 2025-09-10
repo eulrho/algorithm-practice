74e34d9844002df889bd27ef79bb44d9daf67ef1
eulrho <sunriselss19@gmail.com>
2025-09-11 00:25:58 +0900

    1149. RGB거리

---
+++ Baekjoon/silver/1149.cpp
@@ -0,0 +1,24 @@
+// RGB거리
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n; cin >> n;
+	int arr[n+1][3], dp[1001][3] = {0};
+	for (int i=1; i<=n; i++) cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
+
+	dp[1][0] = arr[1][0];
+	dp[1][1] = arr[1][1];
+	dp[1][2] = arr[1][2];
+
+	for (int i=2; i<=n; i++) {
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+	}
+	cout << min(min(dp[n][0], dp[n][1]), dp[n][2]);
+	return (0);
+}
