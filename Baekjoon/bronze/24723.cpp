5562e8e14f393b49c48a699d50ed25dcfc12d871
eulrho <sunriselss19@gmail.com>
2025-10-22 21:10:25 +0900

    24723. 녹색거탑

---
+++ Baekjoon/bronze/24723.cpp
@@ -0,0 +1,13 @@
+// 녹색거탑
+#include <iostream>
+#include <cmath>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n; cin >> n;
+	cout << pow(2, n);
+	return (0);
+}
