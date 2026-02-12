362e54406c1cba34cd23fb2eda4c232d4e793196
eulrho <sunriselss19@gmail.com>
2026-02-12 11:10:31 +0900

    34759. 얼룩말과 사자

---
+++ Baekjoon/bronze/34759.cpp
@@ -0,0 +1,12 @@
+// 얼룩말과 사자
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	long long n; cin >> n;
+	cout << n * 2;
+	return (0);
+}
