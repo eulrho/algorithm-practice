1257fa4142782503bf2d258ed6085b48d7f568da
eulrho <sunriselss19@gmail.com>
2026-02-13 21:49:19 +0900

    44. 사각형 채우기

---
+++ mobius/44.cpp
@@ -0,0 +1,16 @@
+// 사각형 채우기
+#include <iostream>
+#define fast ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	fast;
+	int n; cin >> n;
+	int dp[1001] = {0};
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i=3; i<=n; i++) dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	cout << dp[n];
+	return (0);
+}
