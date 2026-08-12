34286f4f39c028f052a822a712856054db7c37f9
eulrho <sunriselss19@gmail.com>
2026-08-12 23:43:46 +0900

    refactor: 0-20. 주사위 게임 3

--- Programmers/level0/0-20.cpp
+++ Programmers/level0/0-20.cpp
@@ -1,57 +1,45 @@
 // 주사위 게임 3
-#include <iostream>
-#include <cstdlib>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-int case1(int value[]){
-    int p=6;
-    for(int i=0; i<4; i++){
-        if (value[i] < p) p = value[i];
-    }
-
-    return p;
+bool cmp(pair<int, int>& p1, pair<int, int>& p2) {
+	return p1.second > p2.second;
 }
-int case2(int dice[]){
-    int p=0, q=0, r=0;
-    for(int i=1; i<7; i++){
-        if (dice[i] == 2 && p == 0) p = i;
-        else if (dice[i] != 0 && q == 0) q = i;
-        else if (dice[i] != 0 && r == 0) r = i;
-    }
 
-    if(r == 0) return (p+q)*abs(p-q);
-    else return q*r;
-}
-int case3(int dice[]){
-    int p, q;
-    for(int i=1; i<7; i++){
-        if (dice[i] == 3) p = i;
-        else if (dice[i] == 1) q = i;
-    }
-
-    return (10*p+q)*(10*p+q);
-}
 int solution(int a, int b, int c, int d) {
-    int answer = 0, count = 0;
-    int dice[7] = {0,};
-    int value[4] = {a, b, c, d};
-    for(int i=0; i<4; i++){
-        dice[value[i]]++;
-        if(count < dice[value[i]]) count = dice[value[i]];
-    }
+	int answer = 0;
+	int arr[7] = {0};
 
-    if (count == 1) answer = case1(value);
-    else if (count == 2) answer = case2(dice);
-    else if (count == 3) answer = case3(dice);
-    else if (count == 4) answer = 1111*a;
+	arr[a]++;
+	arr[b]++;
+	arr[c]++;
+	arr[d]++;
 
-    return answer;
-}
+	vector<pair<int, int>> tmp;
+	int min_num = 10;
+	for (int i=1; i<=6; i++) {
+		if (arr[i] != 0) {
+			tmp.emplace_back(i, arr[i]);
+			min_num = min(min_num, i);
+		}
+	}
 
-int main() {
-    int a = 6, b = 4, c = 2, d = 5;
-    int result = solution(a, b, c, d);
+	sort(tmp.begin(), tmp.end(), cmp);
 
-    cout << result;
+	int size = (int)tmp.size();
+	if (size == 1)
+		answer = 1111 * tmp[0].first;
+	else if (size == 2) {
+		if (tmp[0].second == 2)
+			answer = (tmp[0].first + tmp[1].first) * abs(tmp[0].first - tmp[1].first);
+		else if (tmp[0].second == 3)
+			answer = (int)pow((10 * tmp[0].first + tmp[1].first), 2);
+	}
+	else if (size == 3)
+		answer = tmp[1].first * tmp[2].first;
+	else answer = min_num;
+	return answer;
 }
