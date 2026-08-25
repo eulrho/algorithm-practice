686b4bbd9f56d5e14b5eaf8c1c6f773839f570b8
eulrho <sunriselss19@gmail.com>
2026-08-25 16:41:21 +0900

    2-5. 최솟값 만들기

---
+++ Programmers/level2/2-5.cpp
@@ -0,0 +1,15 @@
+// 최솟값 만들기
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    int size = (int)A.size();
+    for (int i=0; i<size; i++) answer += A[i] * B[size - i - 1];
+    return answer;
+}
